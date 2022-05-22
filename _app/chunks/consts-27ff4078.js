bf53b3234217219c0efd2824b22dd62b244a3e8d
andronasef <andronasef@users.noreply.github.com>
2022-05-22 22:43:35 +0000

    deploy: 5de410857c9493f98396ceae29eb5d42e6417075

--- _app/chunks/consts-27ff4078.js
+++
@@ -1,1 +0,0 @@
-const e={contact:"/contact",resume:"https://docs.google.com/document/d/1fQ-Cbo6OaAMn_9zkNqomq-WQrHqDcBI2Bgz5-Yz0QlE/",carrerHistory:"https://www.linkedin.com/in/andronasef/details/experience/",achievements:"https://www.linkedin.com/in/andronasef/details/honors/"};export{e as L};
